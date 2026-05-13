a0f2ce486ad56e07986c4aacb7d791782bce70e4
cbhallamudi05 <124886639+cbhallamudi05@users.noreply.github.com>
2026-05-13 19:59:56 +0530

    slash removed from CORS

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors({
     origin: [
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "https://cbhallamudi.com/"
+        "https://cbhallamudi.com"
     ]
 }));
 app.use(express.json());
