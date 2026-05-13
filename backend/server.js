149d7ecbb05eb4bc78b60aa6b435dcfab4684765
cbhallamudi05 <124886639+cbhallamudi05@users.noreply.github.com>
2026-05-13 19:54:52 +0530

    CORS updated

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(cors({
     origin: [
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://cbhallamudi.com/"
     ]
 }));
 app.use(express.json());
